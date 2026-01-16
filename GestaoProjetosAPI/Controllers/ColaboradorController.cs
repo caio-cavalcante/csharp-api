17acd8dfb09e262272ebdd8bfbe294aa1d61e3d4
caio <caio.cav.ara@gmail.com>
2026-01-16 11:19:38 -0300

    feat: add GetPorProjeto endpoint to retrieve collaborators by project ID

--- GestaoProjetosAPI/Controllers/ColaboradorController.cs
+++ GestaoProjetosAPI/Controllers/ColaboradorController.cs
@@ -38,5 +38,24 @@ namespace GestaoProjetosAPI.Controllers
                 return Ok(dados);
             }
         }
+
+        [HttpGet("PorProjeto/{id}")]
+        public async Task<IActionResult> GetPorProjeto(int id)
+        {
+            using (var connection = new SqlConnection(_connectionString))
+            {
+                // o dapper entende que não é texto e então passo o ID recebido na URL como parâmetro
+                var dados = await connection.QueryAsync<Colaborador>(
+                    "sp_BuscarColaboradoresPorProjeto", 
+                    new { ProjetoId = id }, 
+                    commandType: System.Data.CommandType.StoredProcedure
+                );
+
+                if (!dados.Any()) 
+                    return NotFound("Nenhum colaborador encontrado neste projeto.");
+
+                return Ok(dados);
+            }
+        }
     }
 }
